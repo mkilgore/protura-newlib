64f0ea45b57c9145512043cc89afcf64f159e9eb
Matt Kilgore <mattkilgore12@gmail.com>
2016-07-05 10:26:09 -0400

    Add waitpid() syscall wrapper

--- newlib/libc/sys/protura/wait.c
+++ newlib/libc/sys/protura/wait.c
@@ -5,10 +5,24 @@
 #include <stdint.h>
 #include <unistd.h>
 
-int _wait(int *wait_ret)
+pid_t _wait(int *wait_ret)
 {
-    int ret;
+    pid_t ret;
     ret = syscall1(SYSCALL_WAIT, (uint32_t)wait_ret);
+    if (ret < 0) {
+        errno = -ret;
+        return -1;
+    }
+
+    return ret;
+}
+
+pid_t waitpid(pid_t child, int *wstatus, int options)
+{
+    pid_t ret;
+
+    ret = syscall3(SYSCALL_WAITPID, (uint32_t)child, (uint32_t)wstatus, (uint32_t)options);
+
     if (ret < 0) {
         errno = -ret;
         return -1;
