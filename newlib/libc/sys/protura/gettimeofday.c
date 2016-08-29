3ebc5398d5cf30cc9b08e9535ee1118921435670
Matt Kilgore <mattkilgore12@gmail.com>
2016-08-28 23:18:37 -0400

    Implement time() and badly implemented gettimeofday()

--- newlib/libc/sys/protura/gettimeofday.c
+++ newlib/libc/sys/protura/gettimeofday.c
@@ -2,12 +2,38 @@
 #include "syscall.h"
 #include <sys/errno.h>
 #include <sys/types.h>
+#include <sys/time.h>
+#include <time.h>
 #include <stdint.h>
 #include <unistd.h>
 
+time_t time(time_t *t)
+{
+    int ret;
+    time_t tim;
+
+    ret = syscall1(SYSCALL_TIME, (uint32_t)&tim);
+    if (ret) {
+        errno = -ret;
+        return (time_t)-1;
+    }
+
+    if (t)
+        *t = tim;
+
+    return tim;
+}
+
 int _gettimeofday(struct timeval *tv, struct timezone *tz)
 {
-    errno = EINVAL;
-    return -1;
+    time_t t = time(NULL);
+
+    if (t == (time_t)-1)
+        return -1;
+
+    tv->tv_sec = t;
+    tv->tv_usec = 0;
+
+    return 0;
 }
 
